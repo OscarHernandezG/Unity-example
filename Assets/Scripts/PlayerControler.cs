aee24e6688863dbfeb9d7a3cab1838826a52876d
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-09-20 23:05:37 +0200

    Project

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody body;
+    private int count;
+
+	// Use this for initialization
+	void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+
+        count = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        body.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+    }
+}
