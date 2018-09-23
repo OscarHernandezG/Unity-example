f9447631f0a861bcdc6fabe7b94c8e43a91e8242
Óscar Hernández <oscarhernandezgomez@gmail.com>
2018-09-23 19:22:10 +0200

    Ex 2

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public GameObject player;
+    public int speed;
+
+    private Rigidbody body;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 move = -(transform.position - player.transform.position);
+
+        move.y = 0;
+
+        body.AddForce(move * speed);
+    }
+}
